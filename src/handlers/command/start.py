10cfc6206e02980f98f6c861319c2b91df285ed6
drain678 <ekaterina1.zorina@gmail.com>
2024-10-17 19:24:02 +0300

    improved exceptions in start.py

--- src/handlers/command/start.py
+++ src/handlers/command/start.py
@@ -1,5 +1,8 @@
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.exceptions import TelegramAPIError, DetailedAiogramError, TelegramNetworkError, TelegramBadRequest, TelegramUnauthorizedError, TelegramServerError
+import asyncio
+
 
 from src.handlers.command.router import router
 from src.logger import set_correlation_id, logger
@@ -8,9 +11,27 @@ from src.logger import set_correlation_id, logger
 @router.message(Command('start'))
 async def start(message: Message):
     correlation_id = set_correlation_id()
-    logger.info(f"Команда /start получена от пользователя: {message.from_user.id}, Correlation ID: {correlation_id}")
+    logger.info(f'Команда /start получена от пользователя: {message.from_user.id}, Correlation ID: {correlation_id}')
     try:
         await message.answer('Hello')
-        logger.info(f"Сообщение отправлено пользователю {message.from_user.id}")
-    except Exception as e:
-        logger.error(f"Ошибка при отправке сообщения пользователю {message.from_user.id}: {e}")
+        logger.info(f'Сообщение отправлено пользователю {message.from_user.id}')
+    except TelegramBadRequest as e:
+        logger.warning(f'Неправильный запрос {e.message} от пользователя {message.from_user.id}. Correlation ID: {correlation_id}')
+
+    except TelegramUnauthorizedError as e:
+        logger.error(f'Ошибка, токен бота недействителен {e.message}. Пользователь {message.from_user.id}. Correlation ID: {correlation_id}')
+
+    except TelegramNetworkError as e:
+        logger.error(f'Ошибка сети {e.message} при отправке сообщения пользователю {message.from_user.id}. Correlation ID: {correlation_id}')
+
+    except asyncio.TimeoutError:
+        logger.error(f'Время ожидания истекло при отправке сообщения пользователю {message.from_user.id}. Correlation ID: {correlation_id}')
+    
+    except TelegramServerError as e:
+        logger.error(f'Ошибка сервера Telegram {e.message}. Пользователь {message.from_user.id}. Correlation ID: {correlation_id}')
+
+    except TelegramAPIError as e:
+        logger.error(f'Ошибка API Telegram {e.message} при отправке сообщения пользователю {message.from_user.id}. Correlation ID: {correlation_id}')
+    
+    except DetailedAiogramError as e:
+        logger.error(f'Неизвестная ошибка {e.message} при отправке сообщения пользователю {message.from_user.id}. Correlation ID: {correlation_id}')
