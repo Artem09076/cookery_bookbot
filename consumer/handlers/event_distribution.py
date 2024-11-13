955a8e0e746907474b1a77e30294d4016cfb6ae4
drain678 <ekaterina1.zorina@gmail.com>
2024-11-13 17:26:03 +0300

    save local files before git pull

--- consumer/handlers/event_distribution.py
+++ consumer/handlers/event_distribution.py
@@ -1,6 +1,9 @@
 from consumer.handlers.login import login
+from consumer.handlers.all_ids import admin
 
 async def handle_event_distribution(body):
     match body['action']:
         case 'login':
             await login(body)
+        # case 'admin':
+        #     await admin(body)
